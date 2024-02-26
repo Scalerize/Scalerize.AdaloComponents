4904186d54c7ac5c6eaa963f88e5e8231583198e
Frank <frank.libolt@hotmail.com>
2024-02-26 14:37:12 +0100

    Added sparkline

--- Slider/src/components/Slider/index.js
+++ Slider/src/components/Slider/index.js
@@ -1,16 +1,68 @@
-import {useState, useEffect} from 'react';
 import {log, report} from "../../../../Shared/utils";
-import CheckIcon from '@mui/icons-material/Check';
-import CloseIcon from '@mui/icons-material/Close';
 import Slider from '@mui/material/Slider'
-import dayjs from 'dayjs'
-import {environment, environments} from "../../../../Shared/constants";
+import Stack from '@mui/material/Stack'
 
-const Table = (props) => { 
-    const sliderStyle = {
-        width: '100%'
+const Table = (props) => {
+    log(props);
+    /*
+    
+    {
+        "minValue": {value: 5, initial: 5, onChange: ƒ},
+        "maxValue": {value: 5, initial: 5, onChange: ƒ},
+        "Rail": {
+            "color": "#e6e6e6",
+            "thickness": 0
+        },
+        "Track": {
+            "color": "#000000",
+            "thickness": 0
+        },
+        "Thumb": {
+            "color": "#000000",
+            "hasRing": false,
+            "ringColor": "#00000000",
+            "ringThickness": 0,
+            "hasGrip": false
+        },
+        "Value Label": {
+            "color": "#000000",
+            "textColor": "#ffffff"
+        }
     }
-    return <Slider sx={sliderStyle}></Slider>
+    
+     */
+    const style = {
+        wrapper: {
+            width: '100%',
+            height: `${props._height}px`,
+            display: 'flex',
+            'flex-direction': 'column'
+        },
+        sparkline: {
+            height: '100%',
+            flex: 1,
+            display: 'flex',
+            flexDirection: 'row',
+            gap: '2px'
+        },
+        bar: {
+            backgroundColor: `${props.Track.color}`,
+            height: '100%',
+            flex: 1,
+
+        },
+        slider: {
+            width: '100%',
+            marginTop: '-15px'
+        }
+    }
+    return <div style={style.wrapper}>
+        <div style={style.sparkline}>
+            {[...Array(props.Sparkline.subdivisions).keys()]
+                .map(x => <div style={style.bar}></div>)}
+        </div>
+        <Slider sx={style.slider}></Slider>
+    </div>
 };
 
 export default Table;
