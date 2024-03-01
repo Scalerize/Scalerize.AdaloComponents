03423234458f54ad1350cd3fa3ad2f9dabc9ea25
Frank <frank.libolt@hotmail.com>
2024-03-01 18:24:02 +0100

    reimplement slider

--- Slider/src/components/Slider/index.js
+++ Slider/src/components/Slider/index.js
@@ -1,38 +1,100 @@
+import {useState, useRef, useEffect} from 'react';
 import Slider, {SliderThumb} from '@mui/material/Slider'
 import min from 'lodash/min';
 import max from 'lodash/max';
 import zip from 'lodash/zip';
-import {View} from 'react-native';
+import {Animated, PanResponder, StyleSheet, View} from 'react-native';
+import {log} from "../../../../Shared/utils";
 
 const defaultRandomBarChartCollection = [...Array(100).keys()].map(() => 100);
 
-const thumb = (hasGrip, color) => (props) => {
-    const {children, ...other} = props;
-    const style = {
-        div: {
-            display: 'flex',
-            flexDirection: 'row',
-            gap: 2
-        },
-        gripItem: {
-            height: 9,
-            width: 1.5,
-            backgroundColor: color
-        }
-    };
-    return (
-        <SliderThumb {...other}>
-            {children}
-            {hasGrip
-                ? <View style={style.div}>
-                    <View style={style.gripItem}/>
-                    <View style={style.gripItem}/>
-                    <View style={style.gripItem}/>
-                </View>
-                : <></>}
-        </SliderThumb>
-    );
-};
+class Thumb extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            parentWidth: 0
+        };
+    }
+
+    componentDidMount() {
+        const initialTranslate = -this.props.diameter / 2;
+        const initialValue = this.props.thumbType === 'min' ? this.props.rangeStart : this.props.rangeEnd;
+        let translateX = initialValue / this.props.maxValue * this.state.parentWidth;
+
+        this.pan = new Animated.ValueXY({
+            x: translateX + initialTranslate,
+            y: initialTranslate
+        });
+
+
+        this.panResponder = PanResponder.create({
+            onStartShouldSetPanResponder: () => true,
+            onPanResponderMove: Animated.event([null, {dx: this.pan.x}],
+                {
+                    useNativeDriver: true,
+                    listener: (event, gesture) => {
+                        this.props.onValueChange((this.pan.x._offset + gesture.dx) / this.state.parentWidth * this.props.maxValue);
+                    }
+                }),
+            onPanResponderRelease: () => {
+                this.pan.extractOffset();
+            }
+        });
+    }
+
+    onLayout(event) {
+        const {width} = event.nativeEvent.layout;
+        this.setState({parentWidth: width});
+    }
+
+    render() {
+        const initialTranslate = -this.props.diameter / 2;
+        const initialValue = this.props.thumbType === 'min' ? this.props.rangeStart : this.props.rangeEnd;
+        let translateX = initialValue / this.props.maxValue * this.state.parentWidth;
+        let range = [initialTranslate - translateX, this.state.parentWidth + initialTranslate - translateX];
+        const innerStyle = StyleSheet.create({
+            div: {
+                display: 'flex',
+                flexDirection: 'row',
+                gap: 2
+            },
+            gripItem: {
+                height: 9,
+                width: 1.5,
+                backgroundColor: this.props.gripColor
+            }
+        });
+
+        return (<View
+                onLayout={this.onLayout.bind(this)}>
+                {this.pan && this.panResponder && this.state.parentWidth > 0
+                    ? <Animated.View style={[this.props.style, {
+                        transform: [{
+                            translateX: this.pan.x.interpolate({
+                                inputRange: range,
+                                outputRange: range,
+                                extrapolate: 'clamp'
+                            })
+                        },
+                            {
+                                translateY: this.pan.y
+                            }]
+                    }]}
+                                     {...this.panResponder.panHandlers}>
+                        {this.props.children}
+                        {this.props.hasGrip
+                            ? <View style={innerStyle.div}>
+                                <View style={innerStyle.gripItem}/>
+                                <View style={innerStyle.gripItem}/>
+                                <View style={innerStyle.gripItem}/>
+                            </View>
+                            : <></>}
+                    </Animated.View>
+                    : <></>}
+            </View>
+        );
+    }
+}
 
 const SparklineSlider = (props) => {
     let collection = props.barChartCollection?.map(x => x?.barChartValueSelector);
@@ -41,6 +103,11 @@ const SparklineSlider = (props) => {
         min([min(collection), props.rangeStart.initial]),
         max([max(collection), props.rangeEnd.initial])
     ];
+
+    const [value, setValue] = useState({
+        rangeStart: props.rangeStart.initial,
+        rangeEnd: props.rangeEnd.initial
+    })
     const computeDimensionAndColor = (props) => {
         let initial = [...Array(props.Sparkline.subdivisions).keys()];
 
@@ -72,9 +139,12 @@ const SparklineSlider = (props) => {
         return [colorArray, heightArray];
     }
 
-    const onChange = (e) => {
-        props.rangeStart.onChange(e.target.value[0]);
-        props.rangeEnd.onChange(e.target.value[1]);
+    const onStartChange = (e) => {
+        props.rangeStart.onChange(e);
+    }
+
+    const onEndChange = (e) => {
+        props.rangeEnd.onChange(e);
     }
 
     const style = {
@@ -150,6 +220,64 @@ const SparklineSlider = (props) => {
             },
         }
     }
+
+    const styles2 = StyleSheet.create({
+        container: {
+            position: 'relative',
+            paddingTop: 13,
+            paddingBottom: 13,
+            paddingLeft: 0,
+            paddingRight: 0
+        },
+        railWrapper: {
+            position: 'relative'
+        },
+        rail: {
+            position: 'absolute',
+            width: '100%',
+            backgroundColor: props.Rail.color,
+            height: `${props.Rail.thickness}px`,
+        },
+        track: {
+            position: 'absolute',
+            backgroundColor: props.Track.color,
+            height: `${props.Track.thickness}px`,
+            left: `${value.rangeStart / maxValue * 100}%`,
+            right: `${(maxValue - value.rangeEnd) / maxValue * 100}%`,
+        },
+        thumb: {
+            position: "absolute",
+            height: `${props.Thumb.diameter}px`,
+            width: `${props.Thumb.diameter}px`,
+            backgroundColor: `${props.Thumb.color}`,
+            outlineColor: `${props.Thumb.color}`,
+            color: `${props.Thumb.color}`,
+            border: `${(props.Thumb.hasRing ? props.Thumb.ringThickness : 0)}px solid ${props.Thumb.ringColor}`,
+            boxShadow: `${props.Thumb.shadow}px ${props.Thumb.shadow}px ${props.Thumb.shadow}px rgba(0,0,0,0.2)`,
+            borderRadius: '50%',
+            cursor: 'pointer',
+            transform: [
+                {translateX: -props.Thumb.diameter / 2},
+                {translateY: -props.Thumb.diameter / 2},
+            ],
+        },
+        thumbLeft: {
+            left: `${value.rangeStart / maxValue * 100}%`,
+        },
+        thumbRight: {
+            left: `${value.rangeEnd / maxValue * 100}%`,
+        }
+    });
+
+    const thumbBaseProperties = {
+        hasGrip: props.Thumb.hasGrip,
+        gripColor: props.Thumb.ringColor,
+        diameter: props.Thumb.diameter,
+        ...value,
+        minValue,
+        maxValue
+    };
+
     return <View style={style.wrapper}>
         <View style={style.sparkline}>
             {
@@ -158,12 +286,16 @@ const SparklineSlider = (props) => {
                 })
             }
         </View>
-        <Slider sx={style.slider} onChange={onChange}
-                valueLabelDisplay={props.valueLabel.enabled ? 'on' : 'off'}
-                key={`${props.rangeStart.initial}-${props.rangeStart.initial}`}
-                defaultValue={[props.rangeStart.initial, props.rangeEnd.initial]}
-                slots={{thumb: thumb(props.Thumb.hasGrip, props.Thumb.ringColor)}}
-                min={minValue} max={maxValue} disableSwap></Slider>
+        <View style={styles2.container}>
+            <View style={styles2.railWrapper}>
+                <View style={styles2.rail}></View>
+                <View style={styles2.track}></View>
+                <Thumb {...thumbBaseProperties} thumbType="min" onValueChange={onStartChange}
+                       style={{...styles2.thumb, ...styles2.thumbLeft}}></Thumb>
+                <Thumb {...thumbBaseProperties} thumbType="max" onValueChange={onEndChange}
+                       style={{...styles2.thumb, ...styles2.thumbRight}}></Thumb>
+            </View>
+        </View>
     </View>
 };
 
