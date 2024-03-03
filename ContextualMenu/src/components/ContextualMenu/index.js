465bef9cfc52c349f6d56d9532f2d428f7eb58c0
Frank <frank.libolt@hotmail.com>
2024-03-03 20:51:58 +0100

    Wip slider fix

--- ContextualMenu/src/components/ContextualMenu/index.js
+++ ContextualMenu/src/components/ContextualMenu/index.js
@@ -1,14 +1,11 @@
 import {useState} from 'react';
-import IconButton from '@mui/material/IconButton';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
 import ListItemText from '@mui/material/ListItemText';
 import ListItemIcon from '@mui/material/ListItemIcon';
-import * as DynamicIcon from "@mui/icons-material";
 import Divider from '@mui/material/Divider'
-import {report} from "../../../../Shared/utils";
-import {View, StyleSheet} from 'react-native';
+import {View} from 'react-native';
+import {MaterialIcons} from '@expo/vector-icons';
 
 const divStyle = {
     width: '100%',
@@ -31,67 +28,33 @@ const ContextMenu = (props) => {
             action();
         }
     };
-
-    const upperFirst = (str) => {
-        return str.slice(0, 1).toUpperCase() + str.slice(1, str.length);
-    }
-
-    const fixIconNames = (str) => {
-        let baseIconName = str.split(/[-_]/gm).map(upperFirst).join('');
-        let map = {
-            'AddCall': 'AddIcCall',
-            '360': 'ThreeSixty',
-            '5G': 'FiveG',
-            '3dRotation': 'ThreeDRotation',
-            '13mp': 'ThirteenMp',
-            //TODO : Add all Mp variants + all icons starting with numbers
-        };
-        return map[baseIconName] || baseIconName;
-    }
-
+ 
     return (
         <View style={divStyle}>
-            <IconButton
-                id="basic-button"
-                aria-controls={open ? 'basic-menu' : undefined}
-                aria-haspopup="true"
-                aria-expanded={open ? 'true' : undefined}
-                onClick={handleClick}
-                sx={{color: props.color}}
-            >
-                <MoreVertIcon onClick={handleClick}></MoreVertIcon>
-            </IconButton>
+            <MaterialIcons
+                onClick={handleClick} name="more-vert" color={props.color}/>
+            {/* TODO: replace menu */}
             <Menu
-                id="basic-menu"
                 anchorEl={anchorEl}
                 open={open}
                 onClose={handleClose}
-                MenuListProps={{
-                    'aria-labelledby': 'basic-button',
-                }}
-            >{menuItems.flatMap(x => {
-                    const Icon = !!x.icon
-                        ? DynamicIcon[fixIconNames(x.icon)]
-                        : undefined;
+            >
 
-                    if (!!x.icon && !Icon) {
-                        report({
-                            'component': 'contextual-menu',
-                            'missing-icon': `Missing icon mapping : ${x.icon}`
-                        });
-                    }
-
+                {/* TODO: replace menuItem */}
+                {menuItems.flatMap(x => {
                     let elements = [
                         <MenuItem onClick={() => handleClose(x.action)}>
-                            {Icon
+                            {/* TODO: replace listItemIcon */}
+                            {x.icon
                                 ? (<ListItemIcon>
-                                    <Icon fontSize="small"/>
+                                    <MaterialIcons name={x.icon} fontSize="small"/>
                                 </ListItemIcon>)
-                                : ''}
+                                : <></>}
                             <ListItemText>{x.text}</ListItemText>
                         </MenuItem>
                     ];
                     if (x.hasDivider) {
+                        {/* TODO: replace divider */}
                         elements.push(<Divider></Divider>)
                     }
                     return elements;
