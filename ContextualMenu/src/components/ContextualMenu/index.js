246e6b07357b6faba2759f3bc191c726f16043e6
Frank <frank.libolt@hotmail.com>
2024-03-04 10:25:14 +0100

    fix menu

--- ContextualMenu/src/components/ContextualMenu/index.js
+++ ContextualMenu/src/components/ContextualMenu/index.js
@@ -1,27 +1,23 @@
 import {useState} from 'react';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import ListItemText from '@mui/material/ListItemText';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import Divider from '@mui/material/Divider'
 import {View, StyleSheet} from 'react-native';
-import {MaterialIcons} from '@expo/vector-icons';
-// ERROR in ./node_modules/@react-native/assets-registry/registry.js
-//Module build failed (from ./node_modules/babel-loader/lib/index.js):
-//SyntaxError:  registry.js: Support for the experimental syntax 'flow' isn't currently enabled   
+import {
+    IconButton,
+    Divider,
+    Menu
+} from 'react-native-paper';
+
 const ContextMenu = (props) => {
     const menuItems = Object.keys(props)
         .filter(x => x.endsWith('MenuItem'))
         .map(x => props[x])
         .filter(x => !!x?.enabled);
 
-    const [anchorEl, setAnchorEl] = useState(null);
-    const open = Boolean(anchorEl);
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
+    const [open, setOpen] = useState(false);
+    const handleClick = () => {
+        setOpen(true);
     };
     const handleClose = (action) => {
-        setAnchorEl(null);
+        setOpen(false);
         if (typeof (action) === 'function') {
             action();
         }
@@ -35,32 +31,24 @@ const ContextMenu = (props) => {
 
     return (
         <View style={style.div}>
-            <MaterialIcons
-                onClick={handleClick} name="more-vert" color={props.color}/>
-            {/* TODO: replace menu */}
             <Menu
-                anchorEl={anchorEl}
-                open={open}
-                onClose={handleClose}
+                visible={open}
+                onDismiss={handleClose}
+                anchor={
+                    <IconButton
+                        onClick={handleClick} icon="more-vert" iconColor={props.color}/>
+                }
             >
-
-                {/* TODO: replace menuItem */}
                 {menuItems.flatMap(x => {
+                        var props = {
+                            leadingIcon: x.icon || undefined,
+                            title: x.text || undefined
+                        };
                         let elements = [
-                            <MenuItem onClick={() => handleClose(x.action)}>
-                                {/* TODO: replace listItemIcon */}
-                                {x.icon
-                                    ? (<ListItemIcon>
-                                        <MaterialIcons name={x.icon} fontSize="small"/>
-                                    </ListItemIcon>)
-                                    : <></>}
-                                <ListItemText>{x.text}</ListItemText>
-                            </MenuItem>
+                            <Menu.Item onClick={() => handleClose(x.action)} {...props}/>
                         ];
                         if (x.hasDivider) {
-                            {/* TODO: replace divider */
-                            }
-                            elements.push(<Divider></Divider>)
+                            elements.push(<Divider/>)
                         }
                         return elements;
                     }
