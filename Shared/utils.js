542f033103ef7997a88fbd5c07b1ed0a7984804d
Frank <frank.libolt@hotmail.com>
2024-03-04 13:47:55 +0100

    fix deps

--- Shared/utils.js
+++ Shared/utils.js
@@ -20,3 +20,16 @@ export const report = (values) => {
     }
     // TODO: Add a fallback to fetch if needed
 } 
+
+/*
+  import {log} from "../../../../Shared/utils";
+    import AsyncStorage from '@react-native-async-storage/async-storage';
+    useEffect(() => {
+        AsyncStorage.getItem('persist:root').then(
+            (value) => {
+                value = value || window.localStorage.getItem('persist:root');
+                return log(value);
+            }
+        )
+    })
+ */
