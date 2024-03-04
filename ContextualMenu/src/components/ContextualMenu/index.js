f53c3cc9c981fb896508b18e7d464849c591b2ac
Frank <frank.libolt@hotmail.com>
2024-03-04 09:17:35 +0100

    wip

--- ContextualMenu/src/components/ContextualMenu/index.js
+++ ContextualMenu/src/components/ContextualMenu/index.js
@@ -4,13 +4,11 @@ import MenuItem from '@mui/material/MenuItem';
 import ListItemText from '@mui/material/ListItemText';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import Divider from '@mui/material/Divider'
-import {View} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import {MaterialIcons} from '@expo/vector-icons';
-
-const divStyle = {
-    width: '100%',
-    height: '100%'
-}
+// ERROR in ./node_modules/@react-native/assets-registry/registry.js
+//Module build failed (from ./node_modules/babel-loader/lib/index.js):
+//SyntaxError:  registry.js: Support for the experimental syntax 'flow' isn't currently enabled   
 const ContextMenu = (props) => {
     const menuItems = Object.keys(props)
         .filter(x => x.endsWith('MenuItem'))
@@ -28,9 +26,15 @@ const ContextMenu = (props) => {
             action();
         }
     };
- 
+    const style = StyleSheet.create({
+        div: {
+            width: '100%',
+            height: '100%'
+        }
+    });
+
     return (
-        <View style={divStyle}>
+        <View style={style.div}>
             <MaterialIcons
                 onClick={handleClick} name="more-vert" color={props.color}/>
             {/* TODO: replace menu */}
@@ -42,24 +46,25 @@ const ContextMenu = (props) => {
 
                 {/* TODO: replace menuItem */}
                 {menuItems.flatMap(x => {
-                    let elements = [
-                        <MenuItem onClick={() => handleClose(x.action)}>
-                            {/* TODO: replace listItemIcon */}
-                            {x.icon
-                                ? (<ListItemIcon>
-                                    <MaterialIcons name={x.icon} fontSize="small"/>
-                                </ListItemIcon>)
-                                : <></>}
-                            <ListItemText>{x.text}</ListItemText>
-                        </MenuItem>
-                    ];
-                    if (x.hasDivider) {
-                        {/* TODO: replace divider */}
-                        elements.push(<Divider></Divider>)
+                        let elements = [
+                            <MenuItem onClick={() => handleClose(x.action)}>
+                                {/* TODO: replace listItemIcon */}
+                                {x.icon
+                                    ? (<ListItemIcon>
+                                        <MaterialIcons name={x.icon} fontSize="small"/>
+                                    </ListItemIcon>)
+                                    : <></>}
+                                <ListItemText>{x.text}</ListItemText>
+                            </MenuItem>
+                        ];
+                        if (x.hasDivider) {
+                            {/* TODO: replace divider */
+                            }
+                            elements.push(<Divider></Divider>)
+                        }
+                        return elements;
                     }
-                    return elements;
-                }
-            )}
+                )}
             </Menu>
         </View>
 
