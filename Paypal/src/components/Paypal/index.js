683499f3b09dcfb0ed43ccc2dd20fd7d96ea57db
Frank <frank.libolt@hotmail.com>
2024-06-19 20:37:32 +0200

    fixes

--- Paypal/src/components/Paypal/index.js
+++ Paypal/src/components/Paypal/index.js
@@ -2,7 +2,7 @@ import WebView from 'react-native-webview';
 import {Platform} from 'react-native';
 import {useRef, memo} from "react";
 
-const paypalUrl = 'https://paypal-scalerize.flutterflow.app/';
+const paypalUrl = 'https://paypal-scalerize.flutterflow.app/HomePage';
 
 const Paypal = memo((props) => {
     const onUriChange = (url) => {
@@ -19,7 +19,7 @@ const Paypal = memo((props) => {
         }
     };
     const getQueryStringValue = (key, value) => {
-        return encodeURIComponent(key.endsWith('Color') ? value.replace('#', '0xff') : value);
+        return encodeURIComponent(value);
     }
     const buildQueryString = (props) => {
         let queryString = '';
@@ -29,14 +29,12 @@ const Paypal = memo((props) => {
             if (typeof props[key] === 'object' && props[key] != null) {
                 Object.keys(props[key]).forEach(nestedKey => {
                     if (props[key][nestedKey] !== undefined) {
-                        let combinedKey = key + nestedKey.charAt(0).toUpperCase() + nestedKey.slice(1);
-                        const value = getQueryStringValue(combinedKey, props[key][nestedKey]);
-                        queryString += `${encodeURIComponent(combinedKey)}=${value}&`;
+                        let combinedKey = key + nestedKey.charAt(0).toUpperCase() + nestedKey.slice(1); 
+                        queryString += `${encodeURIComponent(combinedKey)}=${encodeURIComponent(props[key][nestedKey])}&`;
                     }
                 });
-            } else if (props[key] != null) {
-                const value = getQueryStringValue(key, props[key]);
-                queryString += `${encodeURIComponent(key)}=${value}&`;
+            } else if (props[key] != null) { 
+                queryString += `${encodeURIComponent(key)}=${encodeURIComponent(props[key])}&`;
             }
         });
         return queryString.slice(0, -1);
@@ -45,6 +43,7 @@ const Paypal = memo((props) => {
     let iframeRef = useRef(null);
     let uri = paypalUrl + '?' + buildQueryString(props);
 
+    // TODO: fix dynamic height
     return Platform.OS === 'web'
         ? <iframe
             style={{width: '100%', height: '100%', borderWidth: 0}}
