c8cd8c9bdeb97c2a46779b9bae8c08792891115d
Frank <frank.libolt@hotmail.com>
2024-08-01 21:45:33 +0200

    Added border property in manifest for certificate

--- PdfGenerator/src/components/InvoiceGenerator/index.js
+++ PdfGenerator/src/components/InvoiceGenerator/index.js
@@ -1,20 +1,22 @@
 import {BaseButton} from "../../../BaseButton";
-import {defaultApiHeaders, urls} from "../../../../Shared/constants"; 
+import {defaultApiHeaders, urls} from "../../../../Shared/constants";
 
 const InvoiceGenerator = (props) => {
-    console.log(props);
-    const getCompany = (company) => ({
-        ...company,
+    const getCompany = (company, prefix) => ({
+        name: company[prefix + 'Name'],
+        email: company[prefix + 'Email'],
+        phone: company[prefix + 'Phone'],
+        vatNumber: company[prefix + 'VatNumber'],
         address: {
-            street: company.address.street,
-            city: company.address.city,
-            country: company.address.country,
-            zipCode: company.address.zipCode,
+            street: company[prefix + 'Street'],
+            city: company[prefix + 'City'],
+            country: company[prefix + 'Country'],
+            zipCode: company[prefix + 'ZipCode'],
         },
     })
 
     const newProps = {
-        ...props.button, 
+        ...props.button,
         _height: props._height,
         generateDocument: async () => {
             const response = await fetch(`${urls.basePdfUrl}accounting-document`, {
@@ -22,8 +24,8 @@ const InvoiceGenerator = (props) => {
                 headers: defaultApiHeaders,
                 body: JSON.stringify({
                     ...props,
-                    company: getCompany(props.company),
-                    client: getCompany(props.client),
+                    company: getCompany(props.company, 'company'),
+                    client: getCompany(props.client, 'client'),
                 }),
             });
 
