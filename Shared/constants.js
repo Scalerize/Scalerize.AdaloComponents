0b48c465d702144d3c0388232aa8d12ff283e2dc
Frank <frank.libolt@hotmail.com>
2024-08-03 12:30:26 +0200

    fix domain

--- Shared/constants.js
+++ Shared/constants.js
@@ -1,5 +1,5 @@
 ﻿
-export const baseUrlDomain = 'https://1cfb-2a01-e0a-5f8-6f80-ac88-e632-c012-4617.ngrok-free.app/';
+export const baseUrlDomain = 'https://www.scalerize.fr/';
 export const urls = {
     beacon: `${baseUrlDomain}automation/webhook/4c7eb7a3-89ae-4bdb-9afd-199e36b177c8`,
     basePdfUrl: `${baseUrlDomain}client-endpoints/scalerize/pdf/`
