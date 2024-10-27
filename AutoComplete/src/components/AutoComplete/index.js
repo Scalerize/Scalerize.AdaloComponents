45011b1c81f535aff738734980c6ff3a7fa47d52
Frank <frank.libolt@hotmail.com>
2024-10-27 08:57:15 +0100

    updated manifest.json

--- AutoComplete/src/components/AutoComplete/index.js
+++ AutoComplete/src/components/AutoComplete/index.js
@@ -2,47 +2,7 @@ import React, { useState } from 'react';
 import { View, TextInput, FlatList, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 const AutoComplete = ({ suggestions }) => {
-	const [query, setQuery] = useState('');
-	const [filteredSuggestions, setFilteredSuggestions] = useState([]);
 
-	const handleInputChange = (text) => {
-		setQuery(text);
-		if (text.length > 0) {
-			const filtered = suggestions.filter((suggestion) =>
-				suggestion.toLowerCase().includes(text.toLowerCase())
-			);
-			setFilteredSuggestions(filtered);
-		} else {
-			setFilteredSuggestions([]);
-		}
-	};
-
-	const handleSelectSuggestion = (suggestion) => {
-		setQuery(suggestion);
-		setFilteredSuggestions([]);
-	};
-
-	return (
-		<View style={styles.container}>
-			<TextInput
-				style={styles.input}
-				placeholder="Type something..."
-				value={query}
-				onChangeText={handleInputChange}
-			/>
-			{filteredSuggestions.length > 0 && (
-				<FlatList
-					data={filteredSuggestions}
-					keyExtractor={(item, index) => index.toString()}
-					renderItem={({ item }) => (
-						<TouchableOpacity onPress={() => handleSelectSuggestion(item)}>
-							<Text style={styles.suggestion}>{item}</Text>
-						</TouchableOpacity>
-					)}
-				/>
-			)}
-		</View>
-	);
 };
 
 const styles = StyleSheet.create({
