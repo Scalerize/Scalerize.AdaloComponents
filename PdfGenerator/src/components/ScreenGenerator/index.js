ebebd1bcdb13a0959dafee0a52f3b7dcb821f2e4
Frank <frank.libolt@hotmail.com>
2025-01-16 09:23:11 +0100

    Add screen generator

---
+++ PdfGenerator/src/components/ScreenGenerator/index.js
@@ -0,0 +1,8 @@
+import {BaseButton} from "../../../BaseButton";
+import {componentsIds, defaultApiHeaders, urls} from "../../../Shared/constants";
+
+const ScreenGenerator = (props) => {
+   <></>
+};
+
+export default ScreenGenerator;
