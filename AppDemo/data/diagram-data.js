eb7c31c35be64f941100b1d8f0ac0f4d7cda93e6
Frank <frank.libolt@hotmail.com>
2025-01-19 18:55:59 +0100

    updated diagrams

--- AppDemo/data/diagram-data.js
+++ AppDemo/data/diagram-data.js
@@ -1,7 +1,7 @@
 export const diagramTestProps = {
     diagramOrientation: "TB",      // "LR", "RL", "TB", or "BT"
-    nodeWidth: 10,
-    nodeHeight: 10,
+    nodeWidth: 20,
+    nodeHeight: 20,
     horizontalSpacing: 60,
     verticalSpacing: 60,
     backgroundColor: "#ffffff",
@@ -18,7 +18,7 @@ export const diagramTestProps = {
         innerFormBorderThickness: 0,
         rotation: 0,
         legendType: "inside",          // "inside", "outside-simple", or "outside-block"
-        legendText: "",
+        legendText: "A",
         legendColor: "#000",
         legendBorderColor: "#333",
         legendBorderThickness: 1,
@@ -37,7 +37,7 @@ export const diagramTestProps = {
         innerFormBorderThickness: 0,
         rotation: 0,
         legendType: "outside-simple", 
-        legendText: "",
+        legendText: "B",
         legendColor: "#000",
         legendBorderColor: "#333",
         legendBorderThickness: 1,
@@ -46,6 +46,25 @@ export const diagramTestProps = {
       },
       {
         nodeId: "C",
+        borderRadius: 100,
+        color: "red",
+        borderColor: "#000000",
+        borderThickness: 2,
+        innerFormBorderColor: "#9E9E9E",
+        innerFormColor: "#FFFFFF00",
+        innerFormBorderRadius: 100,
+        innerFormBorderThickness: 0,
+        rotation: 0,
+        legendType: "outside-block",
+        legendText: "C",
+        legendColor: "#000",
+        legendBorderColor: "#333",
+        legendBorderThickness: 1,
+        legendBorderRadius: 2,
+        legendFillColor: "#FFF"
+      },
+      {
+        nodeId: "D",
         borderRadius: 100,
         color: "red",
         borderColor: "#000000",
@@ -72,18 +91,28 @@ export const diagramTestProps = {
         color: "#000000",
         dotted: false,
         arrow: true,
-        curved: false,
+        curved:     false,
         edgeText: "Edge A-B"
       },
       {
-        fromNodeId: "B",
+        fromNodeId: "A",
         toNodeId: "C",
         thickness: 2,
         color: "#000000",
         dotted: true,
         arrow: true,
-        curved: true,
-        edgeText: "Dotted L-Edge"
+        curved:  false,
+        edgeText: "Edge B-C"
+      },
+      {
+        fromNodeId: "A",
+        toNodeId: "D",
+        thickness: 2,
+        color: "#000000",
+        dotted: true,
+        arrow: true,
+        curved: false,
+        edgeText: "Edge A-D"
       }
     ]
   };
